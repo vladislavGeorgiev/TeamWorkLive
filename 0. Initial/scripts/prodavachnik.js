2b67bdc4a465c9ae5d8fdc4c874df212a12d8290
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-10-20 11:27:47 +0300

    Task 4-2

--- 0. Initial/scripts/prodavachnik.js
+++ 0. Initial/scripts/prodavachnik.js
@@ -12,6 +12,18 @@ function startApp() {
     // Bind the form submit buttons
     $("#buttonLoginUser").click(loginUser);
     $("#buttonRegisterUser").click(registerUser);
+
+    // Bind the info / error boxes
+    $("#infoBox, #errorBox").click(function () {
+        $(this).fadeOut();
+    });
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function () { $("#loadingBox").show() },
+        ajaxStop: function () { $("#loadingBox").hide() }
+    });
+
 	
 	const kinveyBaseUrl = "https://mock.api.com/";
     const kinveyAppKey = "kid_rk";
@@ -40,6 +52,29 @@ function startApp() {
             $("#linkLogout").show();
         }
     }
+
+    function showInfo(message) {
+        $('#infoBox').text(message);
+        $('#infoBox').show();
+        setTimeout(function () {
+            $('#infoBox').fadeOut();
+        }, 3000);
+    }
+
+    function showError(errorMsg) {
+        $('#errorBox').text("Error: " + errorMsg);
+        $('#errorBox').show();
+    }
+
+    function handleAjaxError(response) {
+        let errorMsg = JSON.stringify(response);
+        if (response.readyState === 0)
+            errorMsg = "Cannot connect due to network error.";
+        if (response.responseJSON && response.responseJSON.description)
+            errorMsg = response.responseJSON.description;
+        showError(errorMsg);
+    }
+
 
     function showHomeView() {
         showView('viewHome');
@@ -71,13 +106,15 @@ function startApp() {
             url: kinveyLoginUrl,
             headers: kinveyAuthHeaders,
             data: userData,
-            success: loginSuccess
+            success: loginSuccess,
+            error: handleAjaxError
         });
 
         function loginSuccess(userInfo) {
             saveAuthInSession(userInfo);
             showHideMenuLinks();
             listAdverts();
+            showInfo('Login successful.');
         }
     }
 
@@ -105,14 +142,16 @@ function startApp() {
             url: kinveyRegisterUrl,
             headers: kinveyAuthHeaders,
             data: userData,
-            success: registerSuccess
+            success: registerSuccess,
+            error: handleAjaxError
         });
 
         function registerSuccess(userInfo) {
             console.log(userInfo);
             saveAuthInSession(userInfo);
             showHideMenuLinks();
-           listAdverts();
+            listAdverts();
+            showInfo('User registration successful.');
         }
     }
 
@@ -122,6 +161,7 @@ function startApp() {
         $('#loggedInUser').text("");
         showHideMenuLinks();
         showHomeView();
+        showInfo('Logout successful.');
     }
 	    // advertisement/all
     function listAdverts() {
@@ -136,10 +176,12 @@ function startApp() {
             method: "GET",
             url: kinveyAdvertsUrl,
             headers: kinveyAuthHeaders,
-            success: loadAdvertsSuccess
+            success: loadAdvertsSuccess,
+            error: handleAjaxError
         });
 
         function loadAdvertsSuccess(adverts) {
+            showInfo('Advertisements loaded.');
             if (adverts.length === 0) {
                 $('#ads').text('No advertisements available.');
             } else {
