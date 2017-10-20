b19207cdbc34190417942880fe6e49fc848fe577
vladislavGeorgiev <dataibaaa@gmail.com>
2017-10-20 11:26:33 +0300

    Task 4-1 to new branch.

--- 0. Initial/scripts/prodavachnik.js
+++ 0. Initial/scripts/prodavachnik.js
@@ -1,4 +1,11 @@
 function startApp() {
+	
+	    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
+
+	
     showHideMenuLinks();
     showHomeView();
 
@@ -32,12 +39,14 @@ function startApp() {
             $("#linkRegister").show();
 			$("#linkListAds").hide();
             $("#linkLogout").hide();
+			$("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
 			$("#linkListAds").show();
             $("#linkLogout").show();
+			$("#loggedInUser").show();
         }
     }
 
@@ -86,6 +95,10 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+		let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
+
     }
 
     // user/register
